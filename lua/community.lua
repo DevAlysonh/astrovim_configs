8828c073ea68cd0887a832ca316f5aa3c151e7ba
Dev.Alysonh <dev.alysonh@gmail.com>
2024-05-19 20:00:52 -0300

    meu setup astrovim

---
+++ lua/community.lua
@@ -0,0 +1,26 @@
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+  
+  -- Language packs
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.blade" },
+  { import = "astrocommunity.pack.docker" },
+  { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.php" },
+  { import = "astrocommunity.pack.vue" },
+  -- Language packs
+
+  -- Syntax plugins
+  { import = "astrocommunity.syntax.hlargs-nvim" },
+  -- Syntax plugins
+  
+  -- Indentation plugins
+  { import = "astrocommunity.indent.indent-rainbowline" },
+  -- Indentation plugins
+}
